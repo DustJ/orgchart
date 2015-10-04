fb9dd3329258c0bac94ade6ab2afc6e2242064ac
DustJ <dustin@dustinjacobson.com>
2015-10-03 18:03:32 -0600

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name orgchartsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the orgchartsApp
+ */
+angular.module('orgchartsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
